bf86271c0a4cb4fa4a29e11487768913b0f2331b
Bamba Moussa <moussa.bmb98@gmail.com>
2024-04-01 11:07:54 +0200

    fix of firebase on currentPlatform

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -9,17 +11,16 @@ import 'di/injection_container.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (Firebase.apps.isNotEmpty) {
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
           apiKey: "AIzaSyCCtzV9i-ck-u0F0CRteG0c2F8XNvij8c4",
           appId: "1:682523459689:android:2758bbe2fd5290d4fc62f8",
           messagingSenderId: 'sendid',
           projectId: "weather-59ec7",
-        ));
-  } else {
-    Firebase.initializeApp();
-  }
+        ))
+      : Firebase.initializeApp();
+
   await Hive.initFlutter();
   Hive.registerAdapter(WeatherAdapter());
   Hive.registerAdapter(MainWeatherAdapter());
